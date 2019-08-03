fd2865a16277fd05c3881feda34fb190543c8877
aqianger <qiang7472@163.com>
2019-08-04 01:07:15 +0800

    修改日历控件，显示中文

--- source/directives/searchpanel.ts
+++ source/directives/searchpanel.ts
@@ -9,8 +9,13 @@ export default class SearchPanel {
     SelendDate:SelectItemModel;
     selstartdate:Date;
     selenddate:Date;
+    datetitles:any;
     constructor(private scope:any){
         this.selTypes="1";//默认按年统计
+        this.datetitles={"month":{"January":"一月","February":"二月","March":"三月","April":"四月","May":"五月","June":"六月","July":"七月",
+        "August":"八月","September":"九月","October":"十月","November":"十一月","December":"十二月"},
+    "week":{"Sun":"日","Mon":"一","Tue":"二","Wed":"三","Thu":"四","Fri":"五","Sat":"六"
+ }};
     }
     SetSelTypes(){
        let td:Date =new Date();
